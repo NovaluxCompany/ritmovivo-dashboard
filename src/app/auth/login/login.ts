93535af521aed3b53c7e379df72d2a3514b0c75b
JaiderNicolas <jaidernicolassierra0607@gmail.com>
2026-02-10 16:01:13 -0500

    A test was added to the errors(No database)

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styles: ``,
 })
 export class Login {
+
+
+  mostrarError: boolean =false
+
+    login(form: NgForm) {
+      const correoElectronico = form.value.correoElectronico
+      const contraseña = form.value.contraseña
+      if(!correoElectronico){
+        this.mostrarError = true
+      }
+
+      console.log(correoElectronico, contraseña)
+    }
 
 }
