d1024c4768cebc1fc5c851ec57ee645781dcd444
JaiderNicolas <jaidernicolassierra0607@gmail.com>
2026-02-10 16:21:37 -0500

    Password length validator added(With the test, without connecting to the database)

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -9,17 +9,31 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class Login {
 
-
-  mostrarError: boolean =false
+  reintento: boolean = false
+  mostrarError: boolean = false
+  mensajeError: string = ''
 
     login(form: NgForm) {
       const correoElectronico = form.value.correoElectronico
       const contraseña = form.value.contraseña
+
       if(!correoElectronico){
         this.mostrarError = true
+        this.mensajeError = "Usuario o contraseña incorrectos."
       }
+      this.validarContraseña(contraseña)
+      
 
       console.log(correoElectronico, contraseña)
+    }
+
+    validarContraseña(contra: string){
+      const longitud = /^(?=.*[0-9]).{7,}$/;
+      if(!longitud.test(contra)){
+        this.mensajeError = "La contraseña debe tener mas de 6 palabras"
+        return this.mostrarError = true
+      }
+    return
     }
 
 }
