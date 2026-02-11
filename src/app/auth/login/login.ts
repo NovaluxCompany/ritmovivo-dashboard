0cdd6885287e8b04e018998d188faa6922ca2feb
JaiderNicolas <jaidernicolassierra0607@gmail.com>
2026-02-11 18:31:15 -0500

    Loading screen at the time of validating the added request and with logic in the system

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectorRef } from '@angular/core'; // 👈 Importa esto
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { AuthService } from '../auth';
 import { Router } from '@angular/router';
@@ -12,15 +12,15 @@ import { Router } from '@angular/router';
 export class Login {
   showError: boolean = false;
   messageError: string = '';
+  isLoading: boolean = false;
 
   constructor(
     private authService: AuthService,
     private router: Router,
-    private cdr: ChangeDetectorRef // 👈 Inyéctalo aquí
+    private cdr: ChangeDetectorRef
   ) {}
 
   login(form: NgForm) {
-    // Reset inmediato
     this.showError = false;
     this.cdr.detectChanges(); 
 
@@ -29,28 +29,30 @@ export class Login {
       this.showError = true;
       return;
     }
-
+    this.isLoading = true
     const { email, password } = form.value;
     const lengthRegex = /^(?=.*[0-9]).{6,}$/;
 
     if (!email.includes("@") || !email.includes(".com")) {
+      this.isLoading = false
       this.messageError = "Incorrect email format.";
       this.showError = true;
       return;
     }
 
     if (!lengthRegex.test(password)) {
+      this.isLoading = false
       this.messageError = "Password must be 6+ chars.";
       this.showError = true;
       return;
     }
 
-    // Llamada al servicio
     this.authService.loginDB(email, password).subscribe({
       next: () => {
         this.router.navigate(['/prueba']);
       },
       error: (err) => {
+        this.isLoading = false
         console.error("Server error", err);
         this.messageError = "Incorrect email or password.";
         this.showError = true;
