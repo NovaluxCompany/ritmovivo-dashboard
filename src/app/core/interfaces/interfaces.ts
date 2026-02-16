4a8b0d931e8e83a59eb059c64b7f05f7b75059ae
JaiderNicolas <jaidernicolassierra0607@gmail.com>
2026-02-16 18:52:11 -0500

    Rename interfaces to Response-login

--- src/app/core/interfaces/interfaces.ts
+++
@@ -1,3 +0,0 @@
-export interface ResponseLogin{
-    access_token: string
-}
--- src/app/core/service/auth.service.ts
+++ src/app/core/service/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { tap } from 'rxjs/operators'
 import { TokenService} from './token.service'
-import { ResponseLogin } from '../interfaces/interfaces'
+import { ResponseLogin } from '../interfaces/Response-login'
 import { environment } from '../../../environments/environment';
 
 @Injectable({
