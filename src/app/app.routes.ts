a11391dbf9618e6f528c23813fddc6d241f87ab6
JaiderNicolas <jaidernicolassierra0607@gmail.com>
2026-02-17 10:05:11 -0500

    Unstyled course code template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { Login } from './auth/login/login';
-import { PruebaLogin } from './prueba-login/prueba-login';
 import { LoginGuardian } from '../app/core/guard/login-guard';
+import { GestionCursos } from './modules/cursos/components/gestion-cursos/gestion-cursos';
 
 export const routes: Routes = [
     {path: '', component: Login},
-    //{path: 'prueba', component: PruebaLogin, canActivate:[LoginGuardian]}
+    {path: 'cursos', component: GestionCursos, canActivate:[LoginGuardian]}
+
     //{path: '**'}
 ];
