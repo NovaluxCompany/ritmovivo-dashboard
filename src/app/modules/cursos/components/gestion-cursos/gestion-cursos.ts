817aa17f8bcc5b768759cf192c8da7be33e8322f
JaiderNicolas <jaidernicolassierra0607@gmail.com>
2026-02-17 09:51:25 -0500

    All files saved

---
+++ src/app/modules/cursos/components/gestion-cursos/gestion-cursos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-cursos',
+  imports: [],
+  templateUrl: './gestion-cursos.html',
+  styles: ``,
+})
+export class GestionCursos {
+
+}
---
+++ src/app/modules/cursos/components/modal-curso/modal-curso.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-curso',
+  imports: [],
+  templateUrl: './modal-curso.html',
+  styles: ``,
+})
+export class ModalCurso {
+
+}
