de8493919e4e9fdfba57a1673ea7a8eadeb73d27
JaiderNicolas <jaidernicolassierra0607@gmail.com>
2026-02-18 10:57:58 -0500

    Code was added to the course interface

--- src/app/modules/cursos/models/curso.interface.ts
+++ src/app/modules/cursos/models/curso.interface.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class CursoInterface {
-  
+export class CourseInterface {
+  id: number = 0;
+  name: string = '';
+  instructor: string = '';
+  duration: string = '';
+  price: number = 0;
+  color: string = '';
+  day: string = '';
+  time: string = ''
+  location: string = '';
+  gender: string = '';
+  level: string = '';
+  promo2x1: boolean = false;
+  capacity: number = 0;
+  startDate: string = '';
+  active: boolean = false;
 }
