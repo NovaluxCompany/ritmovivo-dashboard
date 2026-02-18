6f2f19cdda86f880c0bb0632a6d3848a6ecf5446
JaiderNicolas <jaidernicolassierra0607@gmail.com>
2026-02-17 20:51:42 -0500

    Alerts were added to the system to add or edit something.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 import { routes } from './app.routes';
 
@@ -8,6 +10,12 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideAnimations(),
+    provideToastr({
+      timeOut: 3000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+    }),
   ]
 };
--- src/app/modules/cursos/components/modal-curso/modal-curso.ts
+++ src/app/modules/cursos/components/modal-curso/modal-curso.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { ModalCursoService } from '../../service/modal-curso.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-modal-curso',
@@ -9,4 +10,41 @@ import { ModalCursoService } from '../../service/modal-curso.service';
 })
 export class ModalCurso {
     public modalService = inject(ModalCursoService)
+    private toastr = inject(ToastrService)
+
+    actionInSystem() {
+    if(this.modalService.currentAction() === 'Create'){
+      this.actionSave()
+    } else {
+      this.actionEdit()
+    }
+
+    this.modalService.closeModal();
+    }
+
+    actionSave(){
+      this.toastr.success('¡Operación de guardado realizada con éxito!', 'Sistema', {
+      timeOut: 2500,
+      progressBar: true,
+      toastClass: 'ngx-toastr !fixed !top-5 !right-5 !z-[9999] !flex !flex-col !w-72 !p-4 !bg-white !rounded-xl !shadow-2xl !border-l-4 !border-green-500',
+      
+      titleClass: '!text-gray-900 !font-bold !text-sm !block !mb-1',
+      
+      messageClass: '!text-gray-600 !text-xs !block',
+    });
+    }
+
+    actionEdit(){
+      this.toastr.success('¡Operación de editado realizada con éxito!', 'Sistema', {
+      timeOut: 2500,
+      progressBar: true,
+      toastClass: 'ngx-toastr !fixed !top-5 !right-5 !z-[9999] !flex !flex-col !w-72 !p-4 !bg-white !rounded-xl !shadow-2xl !border-l-4 !border-green-500',
+      
+      titleClass: '!text-gray-900 !font-bold !text-sm !block !mb-1',
+      
+      messageClass: '!text-gray-600 !text-xs !block',
+    });
+    }
 }
+
+
