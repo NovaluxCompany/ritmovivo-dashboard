e88b0af615533fbbafcf0471b2863dc4a5777338
JaiderNicolas <jaidernicolassierra0607@gmail.com>
2026-02-19 09:02:15 -0500

    Change the management reference in app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './auth/login/login';
 import { LoginGuardian } from '../app/core/guard/login-guard';
-import { GestionCourse } from './modules/cursos/components/management-course/management-course';
+import { ManagementCourse } from './modules/cursos/components/management-course/management-course';
 export const routes: Routes = [
     {path: '', component: Login},
-    {path: 'cursos', component: GestionCourse, canActivate:[LoginGuardian]}
+    {path: 'cursos', component: ManagementCourse, canActivate:[LoginGuardian]}
 
     //{path: '**'}
 ];
