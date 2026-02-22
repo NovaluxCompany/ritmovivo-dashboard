30736ed480e57396c9e514f2e780c78e19591eaf
JaiderNicolas <jaidernicolassierra0607@gmail.com>
2026-02-22 13:21:05 -0500

    Design added to report

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { Login } from './auth/login/login';
 import { LoginGuardian } from '../app/core/guard/login-guard';
 import { ManagementCourse } from './modules/cursos/components/management-course/management-course';
+import { Reports } from './modules/reports/reports';
 export const routes: Routes = [
     {path: '', component: Login},
-    {path: 'cursos', component: ManagementCourse, canActivate:[LoginGuardian]}
-
+    {path: 'cursos', component: ManagementCourse, canActivate:[LoginGuardian]},
+    {path: 'reportes', component: Reports, canActivate:[LoginGuardian]}
     //{path: '**'}
 ];
