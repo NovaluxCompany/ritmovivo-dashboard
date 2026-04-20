8c2dff5aa1e22c7460c9ca38e40f1445d8eb32a9
Jramos <johnatan.r1000@gmail.com>
2026-04-19 21:17:13 -0500

    Fix filter course.

--- src/app/shared/form-reports/form-reports.ts
+++ src/app/shared/form-reports/form-reports.ts
@@ -19,52 +19,18 @@ export class FormReports {
   private _fb = inject(FormBuilder);
 
   filterForm: FormGroup = this._fb.group({
-    time: [''],
-    enrollmentDate: [''],
-    courseName: [''],
+    classId: [''],
     startDate: [''],
-    endDate:  [''],
-    hasCompanion: [''],
-    companionIdentificationNumber: [''],
-    purchaseDate: [''],
-    day: [''],
-    identificationNumber: [''],
-    fullName: [''],
+    endDate: [''],
     location: [''],
-    startTime: [''],
-    endTime: [''],
   });
 
-  ngOnInit(){
+  ngOnInit() {
     this.loadClasses();
   }
 
   save() {
     const formValues = { ...this.filterForm.value };
-
-    if (this.isEnrolled()) {
-      const selectedClass = this.classes().find(c => c.id === formValues.courseName);
-
-      if (selectedClass) {
-        const parts = selectedClass.name.split('-').map(p => p.trim());
-
-        if (parts.length >= 4) {
-          formValues.courseName = parts[0];
-
-          if (!this.filterForm.get('location')?.value) {
-            formValues.location = parts[1];
-          }
-
-        } else {
-          formValues.courseName = selectedClass.name;
-        }
-      }
-    }
-
-    if (formValues.purchaseDate && !formValues.purchaseDateStart) {
-      formValues.purchaseDateStart = formValues.purchaseDate;
-      formValues.purchaseDateEnd = formValues.purchaseDate;
-    }
 
     const finalFilters = Object.fromEntries(
       Object.entries(formValues).filter(([_, value]) =>
@@ -72,35 +38,7 @@ export class FormReports {
       )
     );
 
-    console.log('Filtros Aplicados:', finalFilters);
     this.onSave.emit(finalFilters);
-  }
-
-  private convertTo24Hour(timeStr: string): string {
-    if (!timeStr) return '';
-    let cleanTime = timeStr.toLowerCase()
-      .replace(/\./g, '')
-      .replace(/[\s\u00A0\u202f]/g, '')
-      .trim();
-
-    if (/^\d{1,2}:\d{2}$/.test(cleanTime)) {
-      const [h, m] = cleanTime.split(':');
-      return `${h.padStart(2, '0')}:${m}`;
-    }
-
-    const isPM = cleanTime.includes('pm') || cleanTime.includes('m');
-    const isAM = cleanTime.includes('am');
-
-    const numbersOnly = cleanTime.replace(/[^\d:]/g, '');
-    let [hours, minutes] = numbersOnly.split(':');
-
-    if (!hours || !minutes) return timeStr;
-
-    let h = parseInt(hours, 10);
-    if (isPM && h < 12) h += 12;
-    if (isAM && h === 12) h = 0;
-
-    return `${h.toString().padStart(2, '0')}:${minutes.padStart(2, '0')}`;
   }
 
   clearForm() {
